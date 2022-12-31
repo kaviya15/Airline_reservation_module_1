910f441820c6df13c0063233cb737993ecf10b75
kaviya <kaviyachuddkie07@gmail.com>
2022-12-31 16:17:43 +0530

    city repo completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
-const {PORT} = require("./config/setupFile")
+const {PORT} = require("./config/setupFile");
+const CityRepository = require("./repository/city-repository")
 // CREATE THE INSTANCE OF EXPRESS APP
 
 const createServer = async()=>{
-    const app = express();
-    app.listen(PORT,()=>{
-        console.log(`Server running at port ${PORT}`);
-    })
-
+  const app = express();
+  app.use(express.json());
+  app.listen(PORT, () => {
+    console.log(`Server running at port ${PORT}`);
+  });
 };
 createServer();
--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,18 +1,45 @@
-import  {City} from '../models/index';
+const { City } = require("../models/index");
 
 class CityRepository {
   async createCity({ name }) {
     try {
-      await City.create({ name: name });
+      let new_city = await City.create({ name: name });
+      return new_city;
     } catch (err) {
+      console.log("Error in the creation of city", err);
       throw { err };
     }
   }
-  async deleteCity({ city_id }) {
+
+  async deleteCity(city_id) {
     try {
-      await City.destroy({ where: { id: city_id } });
+      let delete_city = await City.destroy({ where: { id: city_id } });
+      return delete_city;
     } catch (err) {
+      console.log("Error in the deleting city", err);
       throw { err };
+    }
+  }
+
+  async updateCity(city_id, data) {
+    try {
+      let update_city = await City.update(data, {
+        where: {
+          id: city_id,
+        }
+      });
+      return update_city;
+    } catch (err) {
+      console.log("Error in updating city", err);
+    }
+  }
+
+  async getCity(city_id) {
+    try {
+      let get_city = await City.findByPk(city_id);
+      return get_city;
+    } catch (err) {
+      console.log("Error in fetching the city", err);
     }
   }
 }
